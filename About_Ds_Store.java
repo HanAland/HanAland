9f2cdfe1cc443e65eab9be94ef5397d1da733a2a
HanAland <parhyeyo@sheridancollege.ca>
2022-09-19 18:17:18 -0400

    Guide to remove .DS_Store

---
+++ About_Ds_Store.java
@@ -0,0 +1,11 @@
+public class About_Ds_Store 
+{
+    public static void main(String[] args)
+    {
+        //Step by step guide on how to remove .DS_Store from your directory
+
+        System.out.println("Step 1: Open up Terminal");
+        System.out.println("Step 2: In the command line, type - " + "cd to/your/directory");
+        System.out.println("Step 3: type - " + "find . -name '.DS_Store' -type f -delete");
+    }
+}
