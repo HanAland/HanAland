a5b9dd5aca75175ba11007c330637bbd180c14b9
HanAland <parhyeyo@sheridancollege.ca>
2022-09-19 19:41:15 -0400

    create simple arithmetic calculator

---
+++ BasicCalculator.java
@@ -0,0 +1,36 @@
+public class BasicCalculator 
+{
+    public static void main(String[] args)
+    {
+        BasicCalculator myCalculator = new BasicCalculator();
+        
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(45, 11));
+
+    }   
+    public BasicCalculator()
+    {
+        
+    }
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+    public int subtract(int a, int b)
+    {
+        return a-b;
+    }
+    public int multiply(int a, int b)
+    {
+        return a*b;
+    }
+    public int divide(int a, int b)
+    {
+        return a/b;
+    }
+    public int modulo(int a, int b)
+    {
+        return a%b;
+    }
+
+}
