df0011d46cbe8e46f3d59282f2f898099c243351
HanAland <parhyeyo@sheridancollege.ca>
2022-09-20 16:43:02 -0400

    final commit

--- Java/About_Ds_Store.java
+++ Java/About_Ds_Store.java
@@ -6,6 +6,9 @@ public class About_Ds_Store
 
         System.out.println("Step 1: Open up Terminal");
         System.out.println("Step 2: In the command line, type - " + "cd to/your/directory");
-        System.out.println("Step 3: type - " + "find . -name '.DS_Store' -type f -delete");
+        System.out.println("Step 3: type - " + "find . -name .DS_Store -print0 | xargs -0 git gm --ignore-unmatch");
+        System.out.println("Step 4: type - " + "git add .");   // . means add all changes
+        System.out.println("Step 5: type - " + "git commit -m " + "blah blah");
+        System.out.println("Step 6: type - " + "git push origin HEAD:master");
     }
 }
